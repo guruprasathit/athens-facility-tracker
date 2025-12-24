2ecf486be1595729ce05801d75c18e0659d21eb5
guruprasathit <85774905+guruprasathit@users.noreply.github.com>
2025-12-24 11:58:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,22 +7,3 @@ export default defineConfig({
 ```
 
 ---
-
-### **4. .gitignore**
-```
-node_modules
-.pnp
-.pnp.js
-coverage
-build
-dist
-.DS_Store
-.env.local
-.env.development.local
-.env.test.local
-.env.production.local
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
-.vscode
-.idea
